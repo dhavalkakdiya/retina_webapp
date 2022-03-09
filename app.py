27cf15621794c1946a8e41cfe447e32bce9995f6
Dhaval <69308946+dhavalkakdiya@users.noreply.github.com>
2022-03-09 23:11:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -47,8 +47,9 @@ def predict(image):
     model = load_model(classifier_model,compile=False,)
     test_image = image.resize((28,28))
     test_image = preprocessing.image.img_to_array(test_image)
+    test_image = test_image.reshape(28,28,3)
     test_image = test_image / 255.0
-    test_image = np.expand_dims(test_image, axis=1)
+    test_image = np.expand_dims(test_image, axis=0)
     class_names = ['No',
           'Mild',
           'Moderate',
