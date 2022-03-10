b68e285c41d8922fa77498b4297c3c4d176d4b06
Dhaval <69308946+dhavalkakdiya@users.noreply.github.com>
2022-03-10 11:35:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def main():
     file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
     class_btn = st.button("Classify")
     if file_uploaded is not None:    
-        image = Image.open(file_uploaded)
+        image = Image.open(file_uploaded).convert('RGB')
         st.image(image, caption='Uploaded Image', use_column_width=True)
         
     if class_btn:
